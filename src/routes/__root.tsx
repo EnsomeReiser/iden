31877b96d5cbd08ed9d3e6804b0984257498a922
silverian <dyrus1107@gmail.com>
2025-05-30 11:50:01 +0900

    refactor: add linter and format

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,30 +1,30 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
-import '../index.css';
-import logo from '../logo.svg';
-import reactLogo from '../react.svg';
+import "../index.css";
+import logo from "../logo.svg";
+import reactLogo from "../react.svg";
 
 export const Route = createRootRoute({
-  component: () => <App />,
+	component: () => <App />,
 });
 
 export function App() {
-  return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
-        />
-      </div>
+	return (
+		<div className="container relative z-10 mx-auto p-8 text-center">
+			<div className="mb-8 flex items-center justify-center gap-8">
+				<img
+					src={logo}
+					alt="Bun Logo"
+					className="h-36 scale-120 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+				/>
+				<img
+					src={reactLogo}
+					alt="React Logo"
+					className="h-36 p-6 transition-all duration-300 [animation:spin_20s_linear_infinite] hover:drop-shadow-[0_0_2em_#61dafbaa]"
+				/>
+			</div>
 
-      <Outlet />
-    </div>
-  );
+			<Outlet />
+		</div>
+	);
 }
